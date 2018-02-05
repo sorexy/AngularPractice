fe0a7f9ec63611a5eec1e944e731739e069f5305
Richard Rother <richard.rother17@imperial.ac.uk>
2018-02-05 13:40:24 +0000

    initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+      BrowserModule,
+	  MaterialModule,
+	  BrowserAnimationsModule,
+	  FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+//export class = class to be used elsewhere
+
+export class Dish {
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    description: string;
+}
